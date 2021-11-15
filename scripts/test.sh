f291a291fd73b19ff416fa3e9b45fca922f85343
51m0n397 <simbartolini@gmail.com>
2021-11-15 20:11:11 +0100

    changed test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,1 +1,3 @@
-./bin/yscene render --scene tests/opacity/opacity.json --output out/opacity.jpg --samples 64 --resolution 1024 --sampler color
+#./bin/yscene render --scene tests/opacity/opacity.json --output out/opacity.jpg --samples 64 --resolution 1024 --sampler color
+./bin/yscene render --scene tests/shapes4/shapes4.json --output out/shapes4.jpg --samples 64 --resolution 4096 --sampler color
+#./bin/yscene render --scene tests/shapes4/shapes4.json --output out/shapes4.jpg --samples 16 --resolution 8192 --sampler color
