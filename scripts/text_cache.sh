2c14528ae52cff90a109e9d503e4b7c21b012999
51m0n397 <simbartolini@gmail.com>
2022-02-04 12:30:57 +0100

    changed default resolution

--- scripts/text_cache.sh
+++ scripts/text_cache.sh
@@ -3,6 +3,6 @@ do
     for scene in "$folder"/*
     do
         scene_name="$(basename -- $scene)"
-        ./bin/dgram render_text --scene "${scene}/${scene_name}.json" --resolution 1280
+        ./bin/dgram render_text --scene "${scene}/${scene_name}.json"
     done
 done
